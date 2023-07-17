7933621860f896ae9abec0035053a1af939b8424
dabidabidabi <dabin9365@naver.com>
2023-07-17 19:20:58 +0900

    수진이가하라함

--- src/main/java/com/tp/service/MenuService.java
+++ src/main/java/com/tp/service/MenuService.java
@@ -34,7 +34,7 @@ public class MenuService {
 	public void save(Menu menu, MultipartFile file) throws Exception {
 		
 		if(!file.isEmpty()) {
-			String projectPath = System.getProperty("user.dir") +"\\src\\main\\webapp\\resources\\files";
+			String projectPath = "\\src\\main\\webapp\\resources\\files";
 			
 			UUID uuid = UUID.randomUUID();
 			String fileName = uuid+"_"+file.getOriginalFilename();
